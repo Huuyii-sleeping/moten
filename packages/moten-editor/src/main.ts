6a4c375854b5d00af4c94c5c6377524d6c303c54
huuyii <3134846106@qq.com>
2025-09-29 20:11:25 +0800

    feat(components): 登录界面完成

--- packages/moten-editor/src/main.ts
+++ packages/moten-editor/src/main.ts
@@ -11,9 +11,12 @@ import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import { useUserStore } from './stores/user'
 import { getToken } from './utils/store'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(createPinia())
 app.use(ElementPlus)
 app.use<{ platform: 'editor' | 'user' }>(moten, { platform: 'editor' })
